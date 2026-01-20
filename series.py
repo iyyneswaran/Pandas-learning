7ff3ff8f020e50523756e47b1d260e71454ac086
iyyneswaran <pdee5660@gmail.com>
2026-01-20 21:25:11 +0530

    added comments for series.py

--- series.py
+++ series.py
@@ -1,3 +1,6 @@
 import pandas as pd
 s = pd.Series([1, 2, 3, 4, 5])
 print(s)
+
+# datatype integer as we have integer values alone
+# if i add any other value other than integer then the datatype will be changed to object
